1d39b6b9919ee10a5a9193cb92870390b1fba528
jhonatanfernandesss <jhonatan_contato@hotmail.com>
2022-06-29 15:37:13 -0300

    código corrigido atualizado

--- marca_dagua.py
+++ marca_dagua.py
@@ -7,6 +7,13 @@ import shutil
 
 from numpy._distributor_init import filename
 
+# Diretório das imagens originais
+images_path = glob.glob("./images/*.*")
+
+# Cria um diretório para salvar as imagens tratadas
+dir = './temp'       
+os.makedirs(dir)
+
 # Função para redimensionar a imagem de forma proporcional
 def img_resizing(img, aux, aux_img, width_img):
     
@@ -21,13 +28,6 @@ def img_resizing(img, aux, aux_img, width_img):
     resized_img = cv2.resize(img, (int(aux_heigth), int(aux_width)), interpolation=cv2.INTER_AREA)
     
     return resized_img
-
-# Diretório das imagens originais
-images_path = glob.glob("./images/*.*")
-
-# Cria um diretório para salvar as imagens tratadas
-dir = './temp'       
-os.makedirs(dir)
 
 # Tratamento imagem (marca d'água)
 def watermark_tratament(src, watermark, pos=(0, 0), scale=1):
@@ -52,12 +52,14 @@ for img_path in images_path:
         
         # Seleciona uma imagem
         img = cv2.imread(img_path) 
-
         print(img_path)
+        
         # Diretório da imagem que contém a marca d'água e logotipo 
-        watermark_img = cv2.imread('./watermark.png', -1)
+        watermark_img = cv2.imread('./images/img/watermark.png', -1)
+        
         # pega a imagem do catalogo 
-        catalog = cv2.imread('fundo.jpg')
+        catalog = cv2.imread('./images/img/fundo.jpg')
+        
         # Pega a imagem de fundo para receber a imagem redimensionada
         fundo = catalog.copy()
         fundo = cv2.resize(fundo, (640,480), interpolation=cv2.INTER_AREA)
@@ -105,70 +107,6 @@ for img_path in images_path:
         
         # Define a opacidade
         opacity = opacity / 100
-                
-        # Diretório da marca d'água
-        watermark_img = cv2.imread('./images/marcadagua/watermark.png', -1)
-        print(watermark_img.shape)
-        
-        # Pega a imagem do catalogo 
-        catalog = cv2.imread('./images/marcadagua/fundo.jpg')
-        
-        # Cria uma imagem de fundo transparente para receber a imagem redimensionada
-        fundo = catalog.copy()
-        fundo = fundo[0:480,0:320]
-        fundo = cv2.resize(fundo, (640,480), interpolation=cv2.INTER_AREA)
-        
-        # teste, redimensiona para que a imagem seja maior ou menor que o fundo
-        img = cv2.resize(img,(1000, 300),interpolation=cv2.INTER_AREA)
-        
-        # Pega as dimensões da marca d'água
-        heigth_watermark, width_watermark, _ = watermark_img.shape
-        
-        # Pega as dimensões da imagem
-        heigth_img, width_img, _ = img.shape
-        
-        # Verifica em qual padrão a imagem se encaixa, exemplo: a imagem é um retangulo
-        if heigth_img > heigth_watermark and width_img > width_watermark:
-            # Aplica nova dimensão na imagem
-            resized_img = cv2.resize(img, (width_watermark, heigth_watermark), interpolation=cv2.INTER_AREA)
-            
-        elif heigth_img<=heigth_watermark and width_img>width_watermark:
-            # Aplica nova dimensão na imagem
-            resized_img = cv2.resize(img, (width_watermark, heigth_img), interpolation=cv2.INTER_AREA)
-            
-        elif heigth_img>heigth_watermark and width_img<=width_watermark:
-            # Aplica nova dimensão na imagem
-            resized_img = cv2.resize(img, (width_img,heigth_watermark), interpolation=cv2.INTER_AREA)
-           
-        else:
-            # Aplica nova dimensão na imagem
-            resized_img = cv2.resize(img, (width_img, heigth_img), interpolation=cv2.INTER_AREA)
-        
-        # Seleciona a altura e largura da imagem de fundo
-        altura_img, largura_img, _ = fundo.shape
-        
-        # Identifica o meio da imagem
-        center_y = int(altura_img/2)
-        center_x = int(largura_img/2)
-        
-        # Seleciona a altura e largura da imagem do Catalogo
-        altura_img, largura_img, _ = resized_img.shape
-        
-        # Pega do meio da imagem de fundo e diminui metade da imagem do Catalogo para saber onde a imagem vai começar   
-        top_y_img = center_y - int(altura_img/2)
-        left_x_img = center_x - int(largura_img/2)
-        
-        # Soma o início com o tamanho da imagem
-        bottom_y_img = top_y_img + altura_img
-        right_x_img = left_x_img + largura_img
-        
-        # Adiciona a imagem do catalogo com a dimensão correta na imagem de fundo
-        roi = fundo[top_y_img:bottom_y_img, left_x_img:right_x_img]
-        resized_img = cv2.addWeighted(resized_img,1,roi,0,0, resized_img)
-        fundo[top_y_img:bottom_y_img, left_x_img:right_x_img] = resized_img
-        
-        # Define a opacidade
-        opacity = opacity / 100
         
         # Pega imagem redimensionada
         img_resized = fundo.copy()
@@ -199,7 +137,6 @@ for dirname, subdirs, files in os.walk("./temp/"):
     for filename in files:
         zf.write(os.path.join(dirname, filename))
     zf.close()
-
     
     # Remove diretório temporário
     shutil.rmtree('./temp/')
